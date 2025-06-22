22bc39871bfc66752b010d727ab6a55d8c10dcf8
Евгений <eugink82@mail.ru>
2025-06-22 18:48:07 +0300

    6.Добавлен lombok. Сделан рефакторинг сущностей с учетом Lombok. Добавлен конфигурационный файл БД

--- src/main/java/com/example/my_restaurants/model/Restaurant.java
+++ src/main/java/com/example/my_restaurants/model/Restaurant.java
@@ -1,6 +1,19 @@
 package com.example.my_restaurants.model;
 
+import lombok.Getter;
+
+import java.util.Set;
+
 public class Restaurant extends AbstractNamedEntity {
+
+    @Getter
+    private Set<Menu> menus;
+
+    @Getter
+    private Set<Vote> votes;
+
+    public Restaurant() {
+    }
 
     public Restaurant(Integer id, String name) {
         super(id, name);
